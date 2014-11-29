d2efd75bce1c49ad354db1cbd7ff85e6b6bd422f
Tamir Duberstein <tamird@squareup.com>
2014-11-28 16:47:06 -0800

    Whitespace

--- gash.rs
+++ gash.rs
@@ -26,18 +26,18 @@ impl Shell {
             cmd_prompt: prompt_str.to_owned(),
         }
     }
-    
+
     fn run(&mut self) {
         let mut stdin = BufferedReader::new(stdin());
-        
+
         loop {
             print(self.cmd_prompt);
             io::stdio::flush();
-            
+
             let line = stdin.read_line().unwrap();
             let cmd_line = line.trim().to_owned();
             let program = cmd_line.splitn(' ', 1).nth(0).expect("no program");
-            
+
             match program {
                 ""      =>  { continue; }
                 "exit"  =>  { return; }
@@ -45,17 +45,17 @@ impl Shell {
             }
         }
     }
-    
+
     fn run_cmdline(&mut self, cmd_line: &str) {
         let mut argv: ~[~str] =
             cmd_line.split(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
-    
+
         if argv.len() > 0 {
             let program: ~str = argv.remove(0);
             self.run_cmd(program, argv);
         }
     }
-    
+
     fn run_cmd(&mut self, program: &str, argv: &[~str]) {
         if self.cmd_exists(program) {
             run::process_status(program, argv);
@@ -63,7 +63,7 @@ impl Shell {
             println!("{:s}: command not found", program);
         }
     }
-    
+
     fn cmd_exists(&mut self, cmd_path: &str) -> bool {
         let ret = run::process_output("which", [cmd_path.to_owned()]);
         return ret.expect("exit code error.").status.success();
@@ -73,19 +73,19 @@ impl Shell {
 fn get_cmdline_from_args() -> Option<~str> {
     /* Begin processing program arguments and initiate the parameters. */
     let args = os::args();
-    
+
     let opts = ~[
         getopts::optopt("c")
     ];
-    
+
     let matches = match getopts::getopts(args.tail(), opts) {
         Ok(m) => { m }
         Err(f) => { fail!(f.to_err_msg()) }
     };
-    
+
     if matches.opt_present("c") {
         let cmd_str = match matches.opt_str("c") {
-                                                Some(cmd_str) => {cmd_str.to_owned()}, 
+                                                Some(cmd_str) => {cmd_str.to_owned()},
                                                 None => {~""}
                                               };
         return Some(cmd_str);
@@ -96,7 +96,7 @@ fn get_cmdline_from_args() -> Option<~str> {
 
 fn main() {
     let opt_cmd_line = get_cmdline_from_args();
-    
+
     match opt_cmd_line {
         Some(cmd_line) => Shell::new("").run_cmdline(cmd_line),
         None           => Shell::new("gash > ").run()
